d597affcbb8d460727a3b26a0dfce58ca90abf95
thaleson <thaleson19@hotmail.com>
2024-09-12 11:10:34 -0300

    fix:changes files

--- project/settings.py
+++ project/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['.vercel.app', 'https://recipes-five-silk.vercel.app/', 'localh
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.whiteNoise.Middleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
